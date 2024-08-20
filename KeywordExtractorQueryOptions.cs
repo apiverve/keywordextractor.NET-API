24ae3fd924872d389e70756add7106686d2e4c69
APIVerve Team <hello@apiverve.com>
2024-08-20 10:56:32 -0500

    Bug Fixes

---
+++ KeywordExtractorQueryOptions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class KeywordExtractorQueryOptions {
+public string text { get; set; }
+}
+
+}
